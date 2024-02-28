8c1b5c53342e950f1d763b1d7d94bd878ca5466c
Shi Weigang <shiweigang@loanmon.com>
2024-02-28 17:56:33 +0800

    有理数类

---
+++ Calc24/RationalNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace Calc24;
+/// <summary>
+/// 有理数
+/// </summary>
+public struct RationalNumber
+{
+    /// <summary>
+    /// 分子
+    /// </summary>
+    public int Numerator;
+    /// <summary>
+    /// 分母
+    /// </summary>
+    public int Denominator;
+}
